99f337578848d14d0cb09100b007767978a87d90
MaganaOscar <oscmag13@gmail.com>
2021-06-05 11:39:26 -0700

    fixed methods

--- user.py
+++ user.py
@@ -10,6 +10,8 @@ class User:
         self.account_balance = 0
     def make_withdrawal(self, amount):
         self.account_balance -= int(amount)
+    def make_deposit(self, amount):
+        self.account_balance += amount
     def display_user_balance(self):
         print(f"User: {self.name}, Balance: ${self.account_balance}")
     def transfer_money(self, other_user, amount):
