4b41be6135b1ca5c57cebf95f1f302f717889eb9
MaganaOscar <oscmag13@gmail.com>
2021-06-05 11:54:42 -0700

    added chaining methods

--- user.py
+++ user.py
@@ -10,19 +10,21 @@ class User:
         self.account_balance = 0
     def make_withdrawal(self, amount):
         self.account_balance -= int(amount)
+        return self
     def make_deposit(self, amount):
         self.account_balance += amount
+        return self
     def display_user_balance(self):
         print(f"User: {self.name}, Balance: ${self.account_balance}")
+        return self
     def transfer_money(self, other_user, amount):
         self.account_balance -= amount
         other_user.account_balance += amount
+        return self
 
 guido = User("Guido van Rossum", "guido@python.com")
 monty = User("Monty Python", "monty@python.com")
 print(guido.name)	# output: Guido van Rossum
 print(monty.name)	# output: Monty Python
-guido.display_user_balance()
-guido.make_withdrawal(5)
-guido.display_user_balance()
+guido.make_deposit(100).make_deposit(200).make_deposit(300).make_withdrawal(50).display_user_balance()
 
