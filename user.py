1edfa719c7ae7380365f3aa981540b1ba2e5228e
MaganaOscar <oscmag13@gmail.com>
2021-06-05 11:37:58 -0700

    init commit

---
+++ user.py
@@ -0,0 +1,26 @@
+class User:
+    # class attributes get defined in the class 
+    bank_name = "First National Dojo"
+    # now our method has 2 parameters!
+    def __init__(self, name, email_address):
+    	# we assign them accordingly
+        self.name = name
+        self.email = email_address
+    	# the account balance is set to $0
+        self.account_balance = 0
+    def make_withdrawal(self, amount):
+        self.account_balance -= int(amount)
+    def display_user_balance(self):
+        print(f"User: {self.name}, Balance: ${self.account_balance}")
+    def transfer_money(self, other_user, amount):
+        self.account_balance -= amount
+        other_user.account_balance += amount
+
+guido = User("Guido van Rossum", "guido@python.com")
+monty = User("Monty Python", "monty@python.com")
+print(guido.name)	# output: Guido van Rossum
+print(monty.name)	# output: Monty Python
+guido.display_user_balance()
+guido.make_withdrawal(5)
+guido.display_user_balance()
+
